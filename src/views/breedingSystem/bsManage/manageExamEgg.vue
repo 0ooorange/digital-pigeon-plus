e4cd4ce06b3d4c04a6a0674a6508d7baba41db03
c0dedance <2627702283@qq.com>
2022-04-21 17:35:21 +0800

    feat: 完成产/查/回蛋页面布局

--- src/views/breedingSystem/bsManage/manageExamEgg.vue
+++ src/views/breedingSystem/bsManage/manageExamEgg.vue
@@ -1,15 +1,139 @@
-<template>
-  <div>
-    养殖管理-查蛋
-  </div>
-</template>
-
 <script>
-  export default {
-    name: "examineEggManage"
+import { defineComponent } from 'vue'
+import TableSearch from '@/components/tableSearch/index.vue'
+
+export default defineComponent({
+  name: 'examineEggManage',
+  setup() {
+    const eggsList = [
+      {
+        pigeonnumber: 'A1',
+        boardnumber: '1',
+        actionTime: '2022年4月14日',
+        actionType: '抽蛋',
+        abnormal: '光蛋2',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'A8',
+        boardnumber: '2',
+        actionTime: '2022年4月14日',
+        actionType: '抽蛋',
+        abnormal: '踩蛋2',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'B10',
+        boardnumber: '3',
+        actionTime: '2022年4月14日',
+        actionType: '孵化',
+        abnormal: '光蛋1',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'A15',
+        boardnumber: '4',
+        actionTime: '2022年4月14日',
+        actionType: '孵化',
+        abnormal: '单蛋',
+        principal: '益达',
+        remark: '超过规定天数2天'
+      },
+      {
+        pigeonnumber: 'B1',
+        boardnumber: '5',
+        actionTime: '2022年4月14日',
+        actionType: '抽蛋',
+        abnormal: '无蛋',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'B10',
+        boardnumber: '6',
+        actionTime: '2022年4月14日',
+        actionType: '孵化',
+        abnormal: '踩蛋1',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'C4',
+        boardnumber: '7',
+        actionTime: '2022年4月14日',
+        actionType: '抽蛋',
+        abnormal: '单蛋',
+        principal: '益达',
+        remark: '无'
+      }
+    ]
+    // 查询类型下拉框列表的数据，格式固定
+    const searchTypes = [
+      {
+        value: '鸽笼编号',
+        label: '鸽笼编号'
+      },
+      {
+        value: '面板编号',
+        label: '面板编号',
+        type: ''
+      },
+      {
+        value: '查蛋时间',
+        label: '查蛋时间',
+        type: 'date-picker'
+      },
+      {
+        value: '抽蛋/孵化',
+        label: '抽蛋/孵化',
+        type: 'select'
+      },
+      {
+        value: '异常',
+        label: '异常',
+        type: 'select'
+      },
+      {
+        value: '操作员',
+        label: '操作员'
+      },
+      {
+        value: '备注',
+        label: '备注',
+        type: 'input'
+      }
+    ]
+    const cardList = [
+      {
+        cardText: '总查蛋数',
+        cardNumber: 700
+      }
+    ]
+    return () => (
+      <>
+        <TableSearch searchTypes={searchTypes} cardData={cardList} />
+        <sc-table ref="table" data={eggsList} pageSize={5} stripe highlightCurrentRow>
+          <el-table-column align="center" label="鸽笼编号" prop="pigeonnumber" width="130"></el-table-column>
+          <el-table-column align="center" label="面板编号" prop="boardnumber" width="130"></el-table-column>
+          <el-table-column align="center" label="查蛋时间" prop="actionTime" width="130"></el-table-column>
+          <el-table-column align="center" label="抽蛋/孵化" prop="action" width="130">
+            {{
+              default: ({ row }) => <el-tag type={row.actionType === '抽蛋' ? '' : 'warning'}>{row.actionType}</el-tag>
+            }}
+          </el-table-column>
+          <el-table-column align="center" label="异常" width="130">
+            {{
+              default: ({ row }) => <el-tag type="danger">{row.abnormal}</el-tag>
+            }}
+          </el-table-column>
+          <el-table-column align="center" label="操作员" prop="principal" width="130"></el-table-column>
+          <el-table-column align="center" label="备注" prop="remark"></el-table-column>
+        </sc-table>
+      </>
+    )
   }
+})
 </script>
-
-<style scoped>
-
-</style>
--- src/views/breedingSystem/bsManage/reEggs.vue
+++ src/views/breedingSystem/bsManage/reEggs.vue
@@ -1,15 +1,119 @@
-<template>
-  <div>
-    养殖管理-回蛋
-  </div>
-</template>
-
 <script>
-  export default {
-    name: "reEggsManage"
+import { defineComponent } from 'vue'
+import TableSearch from '@/components/tableSearch/index.vue'
+
+export default defineComponent({
+  name: 'reEggsManage',
+  setup() {
+    const eggsList = [
+      {
+        pigeonnumber: 'A1',
+        boardnumber: '1',
+        actionTime: '2022年4月14日',
+        action: '已取走',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'A8',
+        boardnumber: '2',
+        actionTime: '2022年4月14日',
+        action: '已取走',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'B10',
+        boardnumber: '3',
+        actionTime: '2022年4月14日',
+        action: '已取走',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'A15',
+        boardnumber: '4',
+        actionTime: '2022年4月14日',
+        action: '已取走',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'B1',
+        boardnumber: '5',
+        actionTime: '2022年4月14日',
+        action: '未取走',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'B10',
+        boardnumber: '6',
+        actionTime: '2022年4月14日',
+        action: '已取走',
+        principal: '益达',
+        remark: '无'
+      },
+      {
+        pigeonnumber: 'C4',
+        boardnumber: '7',
+        actionTime: '2022年4月14日',
+        action: '已取走',
+        principal: '益达',
+        remark: '无'
+      }
+    ]
+    // 查询类型下拉框列表的数据，格式固定
+    const searchTypes = [
+      {
+        value: '鸽笼编号',
+        label: '鸽笼编号'
+      },
+      {
+        value: '面板编号',
+        label: '面板编号',
+        type: ''
+      },
+      {
+        value: '回蛋时间',
+        label: '回蛋时间',
+        type: 'date-picker'
+      },
+      {
+        value: '负责人',
+        label: '负责人',
+        type: 'select'
+      },
+      {
+        value: '操作',
+        label: '操作',
+        type: 'select'
+      },
+      {
+        value: '备注',
+        label: '备注',
+        type: 'input'
+      }
+    ]
+    const cardList = [
+      {
+        cardText: '回蛋总数',
+        cardNumber: 1600
+      }
+    ]
+    return () => (
+      <>
+        <TableSearch searchTypes={searchTypes} cardData={cardList} />
+        <sc-table ref="table" data={eggsList} pageSize={5} stripe highlightCurrentRow>
+          <el-table-column align="center" label="鸽笼编号" prop="pigeonnumber" width="130"></el-table-column>
+          <el-table-column align="center" label="面板编号" prop="boardnumber" width="130"></el-table-column>
+          <el-table-column align="center" label="回蛋时间" prop="actionTime" width="130"></el-table-column>
+          <el-table-column align="center" label="负责人" prop="principal" width="130"></el-table-column>
+          <el-table-column align="center" label="操作" prop="action" width="130"></el-table-column>
+          <el-table-column align="center" label="备注" prop="remark"></el-table-column>
+        </sc-table>
+      </>
+    )
   }
+})
 </script>
-
-<style scoped>
-
-</style>
