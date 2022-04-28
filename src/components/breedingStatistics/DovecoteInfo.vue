c02c6adcc9385b39059783319f985f2547e36dbd
TuTu <9869887+aote--man@user.noreply.gitee.com>
2022-04-28 19:41:15 +0800

    fix:修复养殖统计页面

--- src/components/breedingStatistics/DovecoteInfo.vue
+++ src/components/breedingStatistics/DovecoteInfo.vue
@@ -3,42 +3,42 @@
             <div>
                 <el-card class="mini_card">
                     <span class="fontcolor">成鸽数:</span>
-                    <span>234对</span>
+                    <span style=" color:#EE4000">234对</span>
                 </el-card>
             </div>
             <div>
                 <el-card class="mini_card">
                     <span class="fontcolor">幼鸽数:</span>
-                    <span>234对</span>
+                    <span style="color:#EE9A49">234对</span>
                 </el-card>
             </div>
             <div >
                 <el-card class="mini_card">
                     <span class="fontcolor">抽蛋数:</span>
-                    <span>234对</span>
+                    <span style="color:#7D26CD">234对</span>
                 </el-card>
             </div>
             <div>
                 <el-card class="mini_card">
                     <span class="fontcolor"> 产蛋数：</span>
-                     <span>234对</span>
+                     <span style="color: #43CD80">234对</span>
                 </el-card>
             </div>
              <div >
                 <el-card class="mini_card">
                     <span class="fontcolor">出仔数：</span>
-                    <span>234对</span>
+                    <span style="color:#76EE00">234对</span>
                 </el-card>
             </div> <div>
                 <el-card class="mini_card"> 
                     <span class="fontcolor">出栏数：</span>          
-                    <span>234对</span>
+                    <span style="color: #6495ED">234对</span>
                 </el-card>
             </div> 
             <div >
                 <el-card class="mini_card">
                     <span class="fontcolor">死仔数：</span>
-                    <span>234对</span>
+                    <span style=" color:#EE4000">234对</span>
                 </el-card>
             </div>
     
