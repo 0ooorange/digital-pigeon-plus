a3d8f554c0cc3cd737cb0fab96af32e3053446a5
庄珍妮 <9636529+jenny-zhuang@user.noreply.gitee.com>
2022-05-03 01:31:46 +0800

    refactor: 登录页重构

--- src/views/base/login/index.vue
+++ src/views/base/login/index.vue
@@ -6,36 +6,9 @@
         <el-button :class="pswMethodClass" type="text" @click="loginByPsw">密码登录</el-button>
         <el-button :class="messMethodClass" type="text" @click="loginByMess">短信登录</el-button>
       </div>
-      <div class="form" v-if="loginMethod === 0">
-        <div class="inlineForm">
-          <span class="input">
-            <el-input v-model="moblePhoneScrt" placeholder="请输入手机号码/用户名" />
-          </span>
-        </div>
-        <span ref="moblePhoneScrt" class="phoneVerify" style="display: none;">请输入正确的手机号码格式</span>
-        <div class="inlineForm">
-          <span class="input">
-            <el-input v-model="password" type="password" placeholder="请填写密码" show-password />
-          </span>
-        </div>
-        <span class="secretVerify" style="display: none;">密码错误</span>
-        <drag-verify style="margin-top: 30px;"></drag-verify>
-      </div>
-      <div class="form" v-if="loginMethod === 1">
-        <div class="inlineForm">
-          <span class="input">
-            <el-input v-model="moblePhoneMess" placeholder="请输入手机号码" />
-          </span>
-        </div>
-        <span ref="moblePhoneMsg" class="phoneVerify" style="display: none;">请输入正确的手机号码格式</span>
-        <div class="inlineForm">
-          <span class="input verifyInput">
-            <el-input v-model="verify" placeholder="请输入验证码" />
-          </span>
-          <button class="verifyBtn">获取验证码</button>
-        </div>
-        <span class="secretVerify" style="display: none;">验证码错误</span>
-        <drag-verify style="margin-top: 30px;"></drag-verify>
+      <div class="form">
+        <SecretLogin ref="SecretLogin" v-if="loginMethod === 0" />
+        <PswLogin ref="PswLogin" v-if="loginMethod === 1" />
       </div>
       <div class="turnToOther" v-if="loginMethod === 0">
         <el-checkbox class="rememberSecret" v-model="rememberSecret" label="记住密码" size="large" />
@@ -60,101 +33,93 @@
 </template>
 
 <script>
-import weibo from "../../../assets/images/weibo.png";
-import shouji from "../../../assets/images/shouji.png";
-import weixin from "../../../assets/images/weixin.png";
-import zhifubao from "../../../assets/images/zhifubao.png";
+import weibo from '../../../assets/images/weibo.png'
+import shouji from '../../../assets/images/shouji.png'
+import weixin from '../../../assets/images/weixin.png'
+import zhifubao from '../../../assets/images/zhifubao.png'
 import bgImg from '../../../assets/images/loginBgImg.png'
-import dragVerify from './components/dragVerify.vue'
+
+import SecretLogin from './components/secretLogin.vue'
+import PswLogin from './components/pswLogin.vue'
+
+import { ref, nextTick, getCurrentInstance } from 'vue'
+
 export default {
-  components: {
-    dragVerify,
-  },
-  data() {
-    return {
-      weibo: weibo,
-      shouji: shouji,
-      weixin: weixin,
-      zhifubao: zhifubao,
-      bgImg: bgImg,
-      username: '',
-      password: 'admin',
-      loginMethod: 0,
-      moblePhoneScrt: 'admin',
-      moblePhoneMess: '',
-      verify: '',
-      ismoblePhone: true,
-      rememberSecret: false,
-      messMethodClass: 'el-button el-button--text el-button--default selectBtn',
-      pswMethodClass: 'el-button el-button--text el-button--default selectBtn selectBtn-active',
+  components: { SecretLogin, PswLogin },
+  setup() {
+    const { proxy } = getCurrentInstance()
+
+    const rememberSecret = ref(false)
+
+    const loginMethod = ref(0)
+    const messMethodClass = ref('el-button el-button--text el-button--default selectBtn')
+    const pswMethodClass = ref(
+      'el-button el-button--text el-button--default selectBtn selectBtn-active'
+    )
+    // 密码登录
+    const loginByPsw = () => {
+      nextTick(() => {
+        // console.log(proxy.$refs.pswMethod);
+        // console.log(proxy.$refs.messMethod);
+        pswMethodClass.value =
+          'el-button el-button--text el-button--default selectBtn selectBtn-active'
+        messMethodClass.value = 'el-button el-button--text el-button--default selectBtn'
+        loginMethod.value = 0
+      })
     }
-  },
-  watch: {
-    // moblePhoneScrt(newMoblePhone) {
-    //   var moblePhoneReg = /^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
-    //   if (moblePhoneReg.test(newMoblePhone)) {
-    //     this.$refs.moblePhoneScrt.style.display = 'none'
-    //   } else {
-    //     this.$refs.moblePhoneScrt.style.display = 'block'
-    //   }
-    // },
-    moblePhoneMess(newMoblePhone) {
-      var moblePhoneReg = /^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
-      if (moblePhoneReg.test(newMoblePhone)) {
-        this.$refs.moblePhoneMsg.style.display = 'none'
-      } else {
-        this.$refs.moblePhoneMsg.style.display = 'block'
+    // 短信登录
+    const loginByMess = () => {
+      nextTick(() => {
+        // console.log(proxy.$refs.pswMethod)
+        // console.log(proxy.$refs.messMethod)
+        messMethodClass.value =
+          'el-button el-button--text el-button--default selectBtn selectBtn-active'
+        pswMethodClass.value = 'el-button el-button--text el-button--default selectBtn'
+        loginMethod.value = 1
+      })
+    }
+
+    // 登录
+    const islogin = ref(true)
+    const login = async function () {
+      // var validate = await proxy.$refs.loginForm.validate().catch(()=>{})
+      // 	if(!validate){ return false }
+      islogin.value = true
+      var data = {
+        username: proxy.$refs.moblePhoneScrt,
+        password: proxy.$refs.password,
       }
-    },
-  },
-  // el-button el-button--text el-button--default selectBtn selectBtn-active
-  methods: {
-    loginByPsw() {
-      this.$nextTick(() => {
-        console.log(this.$refs.pswMethod)
-        console.log(this.$refs.messMethod)
-        this.pswMethodClass =
-          'el-button el-button--text el-button--default selectBtn selectBtn-active'
-        this.messMethodClass = 'el-button el-button--text el-button--default selectBtn'
-        this.loginMethod = 0
+      //获取token
+      var user = await proxy.$API.auth.token.post(data)
+      if (user.code == 200) {
+        proxy.$TOOL.data.set('TOKEN', user.data.token)
+        proxy.$TOOL.data.set('USER_INFO', user.data.userInfo)
+      } else {
+        islogin.value = false
+        proxy.$message.warning(user.message)
+        return false
+      }
+      proxy.$router.replace({
+        path: '/navigator',
       })
-    },
-    loginByMess() {
-      this.$nextTick(() => {
-        console.log(this.$refs.pswMethod)
-        console.log(this.$refs.messMethod)
-        this.messMethodClass =
-          'el-button el-button--text el-button--default selectBtn selectBtn-active'
-        this.pswMethodClass = 'el-button el-button--text el-button--default selectBtn'
-        this.loginMethod = 1
-      })
-    },
-    async login() {
-      // var validate = await this.$refs.loginForm.validate().catch(()=>{})
-			// 	if(!validate){ return false }
+      proxy.$message.success('Login Success 登录成功')
+      islogin.value = false
+    }
 
-				this.islogin = true
-				var data = {
-					username: this.moblePhoneScrt,
-					password: this.password
-				}
-				//获取token
-				var user = await this.$API.auth.token.post(data)
-				if(user.code == 200){
-					this.$TOOL.data.set("TOKEN", user.data.token)
-					this.$TOOL.data.set("USER_INFO", user.data.userInfo)
-				}else{
-					this.islogin = false
-					this.$message.warning(user.message)
-					return false
-				}
-
-				this.$router.replace({
-					path: '/navigator'
-				})
-				this.$message.success("Login Success 登录成功")
-				this.islogin = false
-    },
+    return {
+      bgImg,
+      weibo,
+      shouji,
+      weixin,
+      zhifubao, // 图片
+      rememberSecret, // 记住密码
+      loginMethod, // 登录方式
+      messMethodClass,
+      pswMethodClass, // 登录css
+      loginByPsw,
+      loginByMess,
+      login,
+    }
   },
 }
 </script>
@@ -221,37 +186,6 @@ export default {
     display: flex;
     align-items: center;
     flex-direction: column;
-    .inlineForm {
-      display: flex;
-      .input {
-        margin: 10px 0;
-        display: inline-block;
-        width: 300px;
-      }
-      .verifyInput {
-        width: 200px;
-      }
-      .verifyBtn {
-        margin: 10px 0;
-        height: 40px;
-        width: 100px;
-        cursor: pointer;
-        border: 0;
-        border-radius: 4px;
-        color: white;
-        background-color: #03aefc;
-      }
-    }
-    .phoneVerify,
-    .secretVerify {
-      margin: -6px 0;
-      display: inline-block;
-      align-self: flex-start;
-      height: 12px;
-      font-size: 12px;
-      line-height: 12px;
-      color: #f5514c;
-    }
   }
   .btn {
     position: absolute;
