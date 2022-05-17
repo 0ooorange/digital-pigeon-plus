9a1abefcb66baf65d9636275c96a0c5482bdfa8c
TuTu <9869887+aote--man@user.noreply.gitee.com>
2022-05-17 19:19:56 +0800

    fix:添加个卡片

--- src/views/breeding/auxiliary/examCub/index.vue
+++ src/views/breeding/auxiliary/examCub/index.vue
@@ -1,13 +1,17 @@
 <template>
 
      <div class="container">
-     <table-search
+   
+        <table-search
 			:cardData="cardData"
 			:showSearch="false"
 			:showDatePk="false"
 			@searchClick="searchClick" 
 			@outTable="outTable" 
+      class="title"
 		/>
+     
+     
  
     <el-main class="nopadding">
 						 <scTable ref="table" :data="apiObj"  stripe  highlightCurrentRow 
@@ -36,6 +40,14 @@ import { defineComponent} from "vue";
 			{
 				cardText: "查仔个数",
 				cardNumber: "666只",
+			},
+      {
+				cardText: "正常仔个数",
+				cardNumber: "566只",
+			},
+      {
+				cardText: "异常仔个数",
+				cardNumber: "466只",
 			},
 		];
       let apiObj=[];
@@ -85,95 +97,12 @@ import { defineComponent} from "vue";
 	return {
 		cardData,
 		apiObj,
-
 		searchClick,
 		 outTable
 	}
      
     }
-    // data() {
-    //   return {
-    //     pageSize:8,
-    //     total:100,
-    //     pageNum:1,
-    //     apiObj:[
-    //         {
-    //             pigeonnumber: "A05",
-    //             boardnumber: "3",
-    //             raweggstime: "2022-4-11 20:21:23",
-    //             incubationdays: "19天",
-    //             operator: "李暖暖",
-    //             remark: "XXXXX"
-    //         } ,
-    //         {
-    //             pigeonnumber: "A03",
-    //             boardnumber: "3",
-    //             raweggstime: "2022-04-21 20:21:23",                
-    //             incubationdays: "19天",
-    //             operator: "李暖暖",
-    //             remark: "XXXXX"
-    //         } ,
-    //         {
-    //             pigeonnumber: "A06",
-    //             boardnumber: "3",
-    //             raweggstime: "2022-03-01 20:21:23",
-    //             incubationdays: "19天",
-    //             operator: "李暖暖",
-    //             remark: "XXXXX"
-    //         } ,
-    //         {
-    //             pigeonnumber: "A10",
-    //             boardnumber: "3",
-    //             raweggstime: "2022-02-23 20:21:23",             
-    //             incubationdays: "19天",
-    //             operator: "李暖暖",
-    //             remark: "XXXXX"
-    //         } ,
-            
-          
-            
-    //     ],
-    //     // 这是卡片数据数组，一个元素一个卡片，元素超过两个自动渲染到查询模块下方
-    //   cardData: [{
-    //     cardText: '查仔个数',
-    //     cardNumber: "666只"
-    //    },],
-    //   // 查询类型下拉框列表的数据，格式固定
-    //   searchTypes: [{
-    //     value: '鸽笼编号',
-    //     label: '鸽笼编号',
-    //   }, {
-    //     value: '鸽板编号',
-    //     label: '鸽板编号',
-    //   }, {
-    //     value: '日期',
-    //     label: '日期',
-    //   }, {
-    //     value: '仔数',
-    //     label: '仔数',
-    //   }, {
-    //     value: '死仔数',
-    //     label: '死仔数',
-    //   }, {
-    //     value: '负责人',
-    //     label: '负责人',
-    //   }, {
-    //     value: '操作',
-    //     label: '操作',
-    //   }]
-       
-    // }
-    // },
-    // methods:{
-    //    // 表格查询事件
-    // searchClick() {
-    //   console.log("嘻嘻嘻，我被点击啦")
-    // },
-    // // 表格导出事件
-    // outTable() {
-    //   console.log("哈哈哈，我被点击了噢");
-    // }
-    // }
+
   })
 </script>
 
@@ -181,86 +110,15 @@ import { defineComponent} from "vue";
 .container{
   margin:20px 20px
 }
-.container111{
-  margin-left: 10px;
-  display: flex;
-  flex-direction:row;
-}
-.card{
-  margin-left:10px;
-  margin-bottom:20px;
-  width:220px;
-}
-.mini_card {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    height: 60px;
-    font-size:20px;
-    color:rgb(5, 0, 0);
-    margin-right: 20px;
-}
-.search{
-  margin-top: 20px;
-  margin-left: 20px;
-  width: 80%;
-  height: 60px;
+/* .title{
   display: flex;
   flex-direction: row;
-  
 }
-.search2{
-  margin-left: 20px;
-
-}
-.btn{
-   display: flex;
-  flex-direction: row;
-  width: 200px;
-  position: relative;
-  margin-left: 40px;
-  
-}
-.btnright{
-    margin-left: 480px;
-    display: flex;
-    flex-direction: row;
-   
-}
-.topleft{
-  background: cornflowerblue;
-}
-.operate{
-  display: flex;
-  flex-direction: row;
-  margin: 20px 20px;
-  position: relative;
-  text-align: center;
-}
-.operatetip{
-  font-size: 18px;
-  width: 150px;
-  height: 50px;
-  line-height: 50px;
-  border:1px solid royalblue;
-}
-.btn{
-  width: 80px;
-  height: 36px;
-  font-size: 20px;
-}
+.title el-card{
+  width: 100px;
+} */
 .tablestyle{
   font-size: 16px;
 }
-.page {
-  margin-top: 36px;
-  margin-left: 195px;
-}
-.soushuo{
-  color: #fff;
-  background-color: #409eff;
-  border-color:#409eff;
-}
+
 </style>
