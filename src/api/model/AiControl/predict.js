22782b541c790b029a11a8c4d9c3f8a3fd60ce06
庄珍妮 <9636529+jenny-zhuang@user.noreply.gitee.com>
2022-09-24 12:21:56 +0800

    突然发现了bug

--- src/api/model/AiControl/predict.js
+++ src/api/model/AiControl/predict.js
@@ -1,12 +1,16 @@
 import http from "@/utils/request";
 
-export default {
-	getPredict: {
-		url: `/api/v1/predict/`,
-		name: "预测环境",
-		post: async function (data = {}) {
-			console.log("url", this.url,data);
-			return await http.post(this.url + '?feature=' + data.feature + '&data=' + data.data + '&target_length=' + data.target_length);
-		},
-	}
-};
+//export default {
+//	getPredict: {
+//		url: `/api/v1/predict/`,
+//		name: "预测环境",
+//		post: async function (data = {}) {
+//			console.log("url", this.url,data);
+//			return await http.post(this.url + '?feature=' + data.feature + '&data=' + data.data + '&target_length=' + data.target_length);
+//		},
+//	}
+//};
+
+const url = '/api/v1/predict/'
+
+export const getPredict = (data = {}) => http.post(url + '?feature=' + data.feature + '&data=' + data.data + '&target_length=' + data.target_length);
--- src/api/model/equipVideo/envForecast.js
+++ src/api/model/equipVideo/envForecast.js
@@ -2,13 +2,15 @@
 
 import http from "@/utils/request";
 
-export default {
-	getCarbonDioxideData: {
-		url: `/digitalPigeon/environment/enviroment/sensordevice/getSensorByIDTypeTime`,
-		name: "查询环境数据",
-		post: async function (data = {},type = "") {
-			console.log("url", this.url,data);
-			return await http.post(this.url + '?start_time=' + data.start_time + '&end_time=' + data.end_time +  '&shed_id=1518124016571797507' + '&type=' + type);
-		},
-	}
-};
+//export default {
+//	getCarbonDioxideData: {
+//		url: `/digitalPigeon/environment/enviroment/sensordevice/getSensorByIDTypeTime`,
+//		name: "查询环境数据",
+//		post: async function (data = {},type = "") {
+//			console.log("url", this.url,data);
+//			return await http.post(this.url + '?start_time=' + data.start_time + '&end_time=' + data.end_time +  '&shed_id=1518124016571797507' + '&type=' + type);
+//		},
+//	}
+//};
+const url  = '/digitalPigeon/environment/enviroment/sensordevice/getSensorByIDTypeTime'
+export const getCarbonDioxideData = (data = {},type = "") => http.post(url + '?start_time=' + data.start_time + '&end_time=' + data.end_time +  '&shed_id=1518124016571797507' + '&type=' + type)
