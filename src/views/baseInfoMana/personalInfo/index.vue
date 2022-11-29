2d614d2725e5fc89f963d33cde6c0e5a4fcb9a9b
Lalalahhhh <2758523053@qq.com>
2022-11-29 10:40:07 +0800

    style：个人信息

--- src/views/baseInfoMana/personalInfo/index.vue
+++ src/views/baseInfoMana/personalInfo/index.vue
@@ -1,15 +1,140 @@
 <template>
-	<div>
-		个人信息
-	</div>
+  <div>
+    <!-- 个人信息部分 -->
+    <div class="personalInfo">
+      <!-- 个人信息编辑（左边） -->
+      <div class="personalInfo-edit">
+        <!-- 图片 -->
+        <div class="personalInfo-edit-img">
+          <img src="" alt="" />
+        </div>
+        <span>刘双印</span>
+        <!-- 操作按钮 -->
+        <div class="personalInfo-edit-btn">
+          <el-button class="personalInfo-edit-btn-1">编辑信息</el-button>
+          <el-button class="personalInfo-edit-btn-2">修改密码</el-button>
+        </div>
+      </div>
+      <!-- 个人信息详情 -->
+      <div class="personalInfo-detail">
+        <span class="personalInfo-detail-title">个人信息</span>
+        <ul>
+          <li>电话号码：1xxxxxxxxx</li>
+          <li>所在单位：养殖部门-金绿基地-A01鸽棚</li>
+          <li>职位：养殖人员</li>
+          <li>故乡：广东省广州市海珠区仲恺农业工程学院</li>
+          <li>生日：2000年9月12日</li>
+          <li>
+            <span class="intro-title">介绍：</span>
+            <div class="intro-box">负责相关鸽棚的饲料喂养，以及设备调控</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'personalInfo',
-	}
+export default {
+  name: "personalInfo",
+};
 </script>
 
 <style scoped>
-
+/* 整体布局 */
+.personalInfo {
+  margin: 50px auto;
+  width: 750px;
+  height: 430px;
+  background-color: rgb(235, 235, 235);
+  border-radius: 15px;
+  border: 2px solid rgb(151, 151, 151);
+}
+.personalInfo-edit {
+  float: left;
+  text-align: center;
+  width: 280px;
+  height: 100%;
+}
+.personalInfo-detail {
+  float: right;
+  margin: 5% 0;
+  width: 460px;
+  height: 85%;
+  /* background-color: green; */
+  border-left: 2px solid rgb(62, 62, 62);
+}
+/* 左箱子（图片+按钮）样式 */
+/* #region */
+.personalInfo-edit-img {
+  display: block;
+  margin: 60px 80px 0;
+  width: 120px;
+  height: 120px;
+  background-color: white;
+}
+.personalInfo-edit-btn {
+  margin: 30px;
+}
+.personalInfo-edit-btn .el-button {
+  display: block;
+  margin: 0 auto 20px;
+  width: 120px;
+  height: 40px;
+  font-size: 20px;
+  color: black;
+}
+.personalInfo-edit-btn-1 {
+  background-color: rgb(147, 233, 255);
+  border: 2px solid rgb(73, 176, 202);
+}
+.personalInfo-edit-btn-1:hover {
+  background-color: rgb(200, 242, 253);
+  color: rgb(67, 67, 67);
+  transition: all 0.3s;
+}
+.personalInfo-edit-btn-2 {
+  background-color: rgb(196, 149, 149);
+  border: 2px solid rgb(179, 107, 107);
+}
+.personalInfo-edit-btn-2:hover {
+  background-color: rgb(207, 169, 169);
+  color: rgb(67, 67, 67);
+  transition: all 0.3s;
+}
+.personalInfo-edit span {
+  display: block;
+  height: 50px;
+  line-height: 50px;
+  font-size: 28px;
+  font-weight: 600;
+}
+/* #endregion */
+/* 右盒子（详情）样式 */
+/* #region */
+li {
+  padding: 0 20px 15px;
+  font-size: 20px;
+  list-style: none;
+}
+.personalInfo-detail .personalInfo-detail-title {
+  display: block;
+  margin-bottom: 30px;
+  padding-left: 30px;
+  font-size: 21px;
+  font-weight: 600;
+}
+.intro-title {
+  float: left;
+}
+.intro-box {
+  float: left;
+  padding: 0 10px;
+  width: 320px;
+  height: 70px;
+  background-color: rgb(248, 237, 150);
+  border: 2px solid rgb(166, 152, 47);
+  border-radius: 5px;
+}
+/* #endregion */
 </style>
