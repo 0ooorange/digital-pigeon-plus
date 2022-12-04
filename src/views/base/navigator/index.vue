bb59f426d4e9e2885e229fb9bba98100a4fd3834
c0dedance <2627702283@qq.com>
2022-12-04 22:18:37 +0800

    Revert "feat: 系统菜单动态显示"

--- src/views/base/navigator/index.vue
+++ src/views/base/navigator/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="app">
-    <span class="rightTop">
-	  <span>刘双印，欢迎您！</span>
+    <span class="rightTop"><span>刘双印，欢迎您！</span>
       <router-link to="/login">注销</router-link>
     </span>
     <circle-menu ref="ccMenu" :menus="menus"></circle-menu>
@@ -9,163 +8,123 @@
 </template>
 
 <script>
-import { ref } from 'vue'
 import tool from '../../../utils/tool'
 import CircleMenu from './components/circleMenu.vue'
 import { ElMessage } from 'element-plus'
-import { getPersonalInfo, getUserMenu } from '@api/bases/login'
-
-const allMenus = [
-  {
-	id: "1",
-    index: 0,
-    name: '养殖系统',
-    system: 'breeding',
-    icon: 'nav_yangzhi',
-    size: '',
-    path: '/breeding/breedStatistics'
-  },
-
-  {
-    index: 1,
-    name: '环境监控',
-    system: 'equipVideo',
-    icon: 'nav_video',
-    size: '',
-    path: '/equipVideo/envForecast'
-  },
-  {
-	id: "11",
-    index: 2,
-    name: '产品溯源',
-    system: 'toSource',
-    icon: 'nav_block',
-    size: '',
-    path: '/toSource/traceSource'
-  },
-  {
-	id: "10",
-    index: 3,
-    name: '屠宰系统',
-    icon: 'nav_jiagong',
-    size: '',
-    path: '#'
-  },
-  {
-	id: "9",
-    index: 4,
-    name: '加工销售',
-    icon: 'nav_jiagong',
-    size: '',
-    path: '#'
-  },
-  {
-	id: "8",
-    index: 5,
-    name: '物流管理',
-    icon: 'nav_wuliu',
-    size: '',
-    path: '#'
-  },
-  {
-	id: "7",
-    index: 6,
-    name: '种鸽质量检测',
-    icon: 'nav_ai',
-    size: '',
-    path: '#'
-  },
-
-  {
-	id: "6",
-    index: 7,
-    name: '电商平台',
-    icon: 'nav_jiagong',
-    size: '',
-    path: '#'
-  },
-  {
-	id: "5",
-    index: 8,
-    name: '基础信息管理',
-    icon: 'nav_info',
-    system: 'baseInfoMana',
-    size: '',
-    path: '/baseInfoMana/personalInfo'
-  },
-  {
-	id: "4",
-    index: 9,
-    name: '认养管理',
-    icon: 'nav_info',
-    size: '',
-    path: '#'
-  },
-  {
-	id: "3",
-    index: 10,
-    name: 'AI+精准管控',
-    system: 'AIControl',
-    icon: 'nav_ai',
-    size: '',
-    path: '/AIControl/actRcognition/clear'
-  },
-  {
-	id: "2",
-    index: 11,
-    name: '可视化界面',
-    icon: 'nav_see',
-    size: '',
-    path: '/dataVisual'
-  }
-]
+import { getPersonalInfo } from '@api/bases/login'
 
 export default {
   name: 'navigator',
   components: { CircleMenu },
   setup() {
+    const menus = [
+      {
+        index: 0,
+        name: '养殖系统',
+        system: 'breeding',
+        icon: 'nav_yangzhi',
+        size: '',
+        path: '/breeding/breedStatistics',
+      },
+
+      {
+        index: 1,
+        name: '环境监控',
+        system: 'equipVideo',
+        icon: 'nav_video',
+        size: '',
+        path: '/equipVideo/envForecast',
+      },
+      {
+        index: 2,
+        name: '产品溯源',
+        system: 'toSource',
+        icon: 'nav_block',
+        size: '',
+        path: '/toSource/traceSource',
+      },
+      {
+        index: 3,
+        name: '屠宰系统',
+        icon: 'nav_jiagong',
+        size: '',
+        path: '#',
+      },
+      {
+        index: 4,
+        name: '加工销售',
+        icon: 'nav_jiagong',
+        size: '',
+        path: '#',
+      },
+      {
+        index: 5,
+        name: '物流管理',
+        icon: 'nav_wuliu',
+        size: '',
+        path: '#',
+      },
+      {
+        index: 6,
+        name: '种鸽质量检测',
+        icon: 'nav_ai',
+        size: '',
+        path: '#',
+      },
+
+      {
+        index: 7,
+        name: '电商平台',
+        icon: 'nav_jiagong',
+        size: '',
+        path: '#',
+      },
+      {
+        index: 8,
+        name: '基础信息管理',
+        icon: 'nav_info',
+        system: 'baseInfoMana',
+        size: '',
+        path: '/baseInfoMana/personalInfo',
+      },
+      {
+        index: 9,
+        name: '认养管理',
+        icon: 'nav_info',
+        size: '',
+        path: '#',
+      },
+      {
+        index: 10,
+        name: 'AI+精准管控',
+        system: 'AIControl',
+        icon: 'nav_ai',
+        size: '',
+        path: '/AIControl/actRcognition/clear',
+      },
+      {
+        index: 11,
+        name: '可视化界面',
+        icon: 'nav_see',
+        size: '',
+        path: '/dataVisual',
+      },
+    ]
     tool.data.set('IS_GET_ROUTER', false)
     tool.data.set('CURR_MENU_INDEX', 0)
-	const menus = ref(allMenus)
+
     // 获取基本信息
-    getPersonalInfo().then(
-      (res) => {
+    getPersonalInfo().then(res => {
         tool.data.set('USER_INFO', res.data?.user)
-      },
-      (err) => {
+      }, err => {
         ElMessage.warning(err.message)
       }
     )
-    // 获取菜单（不同用户不同菜单）
-    getUserMenu().then((res) => {
-      const authMenu = res.data?.value?.children || []
-	  // 存一下映射
-	  const idMapMenuItem = {}
-	  for(const item of authMenu){
-		idMapMenuItem[item.id] = item
-	  }
-	  // 后端返回的数据合并到总的菜单
-	  menus.value = allMenus
-	  .map(item =>{
-		const { id } = item
-		const authItem = idMapMenuItem[id]
-		if(!authItem){
-			return item
-		}
-		delete authItem.icon
-		return {
-			...item,
-			...authItem,
-			auth: true
-		}
-	  })
-	  .sort((a,b)=> a.sort - b.sort)
-    })
 
     return {
-      menus
+      menus,
     }
-  }
+  },
 }
 </script>
 
